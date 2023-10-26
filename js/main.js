ccfdc0533427963c358e38768b8cdd984316b0e7
judyloveth <judyohwo29@gmail.com>
2023-10-26 21:14:26 +0100

    updated style and js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,11 @@
-function validateEmail(email) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-  }
-  
-  const form = document.getElementById('form');
-  const emailInput = document.getElementById('email');
-  
-  form.addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting
-  
-    const email = emailInput.value;
-  
-    if (validateEmail(email)) {
-      
-      // Perform other actions if the email is valid
-    } else {
-      alert("Invalid email address");
-      // Perform other actions if the email is invalid
-    }
-  });
+// Collapsible navigation menu function
+function toggleNav() {
+  let nav = document.querySelector('.nav');
+  nav.classList.toggle('open');
+}
+
+// Close the menu after click
+function closeMenu() {
+  var nav = document.querySelector('.nav');
+  nav.classList.remove('open');
+}
